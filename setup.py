3ee79fb2413f08005a4921ca043a340e5ce026a9
Markus Wanner <markus@bluegap.ch>
2010-01-17 16:45:41 +0100

    Version bump to 0.1dev

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name    = "dtester",
-    version = "0.0",
+    version = "0.1dev",
     description = "A component based test suite for distributed systems",
     author = "Markus Wanner",
     author_email = "markus@bluegap.ch",
+    url = "http://www.bluegap.ch/projects/dtester",
     license = "Boost Software License v1.0 (BSD like)",
     packages = ["dtester"],
 )
